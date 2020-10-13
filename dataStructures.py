bfb3b149556c1170d93304a4115eb712d4d4c3fd
Noah Johnson <jnoahjohnson@gmail.com>
2020-10-13 14:37:07 -0600

    Finish requirements

--- dataStructures.py
+++ dataStructures.py
@@ -11,32 +11,43 @@ import collections
 
 
 def randomName():
-    asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander",
-                   "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman",
-                   "Singing Bush"]
-
-    iRandomNum = random.randint(0, 8)
-
-    return asCustomers[iRandomNum]
-
-
+	asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander",
+	"Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman",
+	"Singing Bush"]
+	
+	iRandomNum = random.randint(0, 8)
+	
+	return asCustomers[iRandomNum]
+	
+	
 def randomBurgers():
-    return random.randint(1, 20)
-
-
+	return random.randint(1, 20)
+	
+	
 # Queue
 burgerQueue = []
 
 # Create Queue from randomName()
 for num in range(0, 100):
-    burgerQueue.append(randomName())
-
+	burgerQueue.append(randomName())
+	
 # Customers
 customers = {}
 
 # Create Customers from Quere
 for person in burgerQueue:
-    if person not in customers:
-        customers[person] = 0
+        # Initialize Person if they are not in customers
+	if person not in customers:
+		customers[person] = 0
+	
+	customers[person] += randomBurgers()
 
-print(customers)
+# Clear queue
+burgerQueue.clear()
+
+listSortedCustomers = sorted(customers.items(), key=lambda x:x[1], reverse=True)
+
+# Print final list
+
+for customer in listSortedCustomers:
+	print(f'{customer[0]: <30}{customer[1]}')
