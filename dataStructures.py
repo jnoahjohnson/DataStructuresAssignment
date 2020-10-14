546898357e604886b888dd6c2d73255c1ac6bd28
Noah Johnson <jnoahjohnson@gmail.com>
2020-10-14 10:59:27 -0600

    Adjust ljust

--- dataStructures.py
+++ dataStructures.py
@@ -11,43 +11,44 @@ import collections
 
 
 def randomName():
-	asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander",
-	"Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman",
-	"Singing Bush"]
-	
-	iRandomNum = random.randint(0, 8)
-	
-	return asCustomers[iRandomNum]
-	
-	
+    asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander",
+                   "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman",
+                   "Singing Bush"]
+
+    iRandomNum = random.randint(0, 8)
+
+    return asCustomers[iRandomNum]
+
+
 def randomBurgers():
-	return random.randint(1, 20)
-	
-	
+    return random.randint(1, 20)
+
+
 # Queue
 burgerQueue = []
 
 # Create Queue from randomName()
 for num in range(0, 100):
-	burgerQueue.append(randomName())
-	
+    burgerQueue.append(randomName())
+
 # Customers (dictionary)
 dictCustomers = {}
 
 # Create Customers from Quere
 for person in burgerQueue:
-        # Initialize Person if they are not in customers
-	if person not in dictCustomers:
-		dictCustomers[person] = 0
-	
-	dictCustomers[person] += randomBurgers()
+    # Initialize Person if they are not in customers
+    if person not in dictCustomers:
+        dictCustomers[person] = 0
+
+    dictCustomers[person] += randomBurgers()
 
 # Clear queue
 burgerQueue.clear()
 
-listSortedCustomers = sorted(dictCustomers.items(), key=lambda x:x[1], reverse=True)
+listSortedCustomers = sorted(
+    dictCustomers.items(), key=lambda x: x[1], reverse=True)
 
 # Print final list
 
 for customer in listSortedCustomers:
-	print(f'{customer[0].ljust(19)}{customer[1]}')
+    print(f'{customer[0].ljust(20)}{customer[1]}')
